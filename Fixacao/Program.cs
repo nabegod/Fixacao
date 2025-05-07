6786a390e7305ab48d483b25af640d2c9de2349b
ana <anabeatrizsouzaalves94@gmail.com>
2025-05-07 12:56:59 -0300

    operação

--- Fixacao/Program.cs
+++ Fixacao/Program.cs
@@ -1,4 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+    Console.WriteLine("Digite um número: ");
+     int a = int.Parse(Console.ReadLine());
 
 
+    Console.WriteLine("Digite um número: ");
+    int b = int.Parse(Console.ReadLine());
+
+    int soma = a + b;
+    int subtracao = a - b;
+    int multiplicacao = a * b;
+    int divisao = a / b;
+    int resto = a % b;
+
+    Console.WriteLine($"A soma é:" + soma);
+
+    }
+}
